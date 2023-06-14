3ab303b2687904611a3061e0987145067f3926d2
Shiska07 <rautshiska@gmail.com>
2023-06-14 17:09:13 -0500

    add type conversion demo code

--- Exercise Files/Ch03/03_04/ideaproj/ideaproj/src/com/example/java/Main.java
+++ Exercise Files/Ch03/03_04/ideaproj/ideaproj/src/com/example/java/Main.java
@@ -17,5 +17,23 @@ public class Main{
         BigDecimal bSum3 = bdValue.add(bdValue).add(bdValue);
 
         System.out.println("The bigdecimal sum is: " + bSum3.toString());
+
+        /*
+        Converting Datatypes:
+
+        1. Converting a smaller datatype to a bigger type is easy and only requires assignment: For example
+            converting an int to a long type.
+        2. However, converting a larger datatype to a smaller datatype requires casting during assignment. For example
+            converting an int to a byte
+         */
+
+        int iVal = 43;
+        int iVal2 = iVal;  // a copy is made and stored as opposed to the variables having the same address
+
+        // convert to long
+        long lVal = iVal2;
+
+        byte bVal = (byte) iVal2; // requires casting
+
     }
 }
